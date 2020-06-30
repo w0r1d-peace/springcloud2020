e4810d43f388955df7980b660bec4fedbd0c8b0f
w0r1d_peace <278882047@qq.com>
2020-06-30 19:10:51 +0800

    springcloud2020

---
+++ cloud-provider-payment8001/src/main/java/com/islet/springcloud/PaymenMain8001.java
@@ -0,0 +1,17 @@
+package com.islet.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: islet
+ * @date: 2020/6/29 23:41
+ * @description:
+ */
+@SpringBootApplication
+public class PaymenMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymenMain8001.class, args);
+    }
+}
