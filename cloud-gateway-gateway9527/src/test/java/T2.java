bb63b1de54157b96c1dd9088c39ac9f7dddb8bae
w0r1d_peace <278882047@qq.com>
2020-07-17 17:19:42 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author: islet
+ * @date: 2020/7/17 16:41
+ * @description:
+ */
+public class T2 {
+
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+    }
+}
